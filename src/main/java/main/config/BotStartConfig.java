e4e690a739e501057a3ada254a3cf6798b8b268c
megoRU <savin.yura2014@gmail.com>
2022-03-14 12:09:00 +0300

    1. Fix ip

--- src/main/java/main/config/BotStartConfig.java
+++ src/main/java/main/config/BotStartConfig.java
@@ -181,7 +181,7 @@ public class BotStartConfig {
                 serverCount = BotStartConfig.jda.getGuilds().size();
                 TOP_GG_API.setStats(serverCount);
                 BotStartConfig.jda.getPresence().setActivity(Activity.playing(BotStartConfig.activity + serverCount + " guilds"));
-                IOUtils.toString(new URL("http://195.2.81.139:3001/api/push/jjyiWxH1QR?msg=OK&ping="), StandardCharsets.UTF_8);
+                IOUtils.toString(new URL("http://193.163.203.77:3001/api/push/jjyiWxH1QR?msg=OK&ping="), StandardCharsets.UTF_8);
             } catch (Exception e) {
                 Thread.currentThread().interrupt();
                 e.printStackTrace();
