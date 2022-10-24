1d1d7e301b9d3c23a87c70665fb3d8784f064868
megoRU <savin.yura2014@gmail.com>
2022-10-24 13:37:25 +0300

    Fix

--- src/main/java/main/hangman/impl/ImageURL.java
+++ src/main/java/main/hangman/impl/ImageURL.java
@@ -3,6 +3,6 @@ package main.hangman.impl;
 public interface ImageURL {
 
     static String get(int count) {
-        return String.format("https://megoru.ru/hangman3/%s.png", count);
+        return String.format("https://megoru.ru/hangman4/%s.png", count);
     }
 }
