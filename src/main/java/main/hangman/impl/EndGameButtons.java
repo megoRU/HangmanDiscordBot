2439ed3caf851d97c2292aec71280df0c605995d
megoRU <savin.yura2014@gmail.com>
2022-11-04 22:19:12 +0300

    patch-12
    1. Fix #217
    2. Fix #212

--- src/main/java/main/hangman/impl/EndGameButtons.java
+++ src/main/java/main/hangman/impl/EndGameButtons.java
@@ -2,6 +2,7 @@ package main.hangman.impl;
 
 import main.config.BotStartConfig;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -10,33 +11,25 @@ public interface EndGameButtons {
 
     static List<Button> getListButtons(long userIdLong) {
         List<Button> buttonList = new LinkedList<>();
-
         buttonList.add(ButtonIMpl.BUTTON_PLAY_AGAIN);
-
-        if (BotStartConfig.getMapGameLanguages().get(userIdLong).equals("eng")) {
-            buttonList.add(ButtonIMpl.BUTTON_RUSSIAN);
-        } else {
-            buttonList.add(ButtonIMpl.BUTTON_ENGLISH);
-        }
-
-        buttonList.add(ButtonIMpl.BUTTON_MY_STATS);
-
-        return buttonList;
+        return getButtons(userIdLong, buttonList);
     }
 
     static List<Button> getListButtons(long userIdLong, long secondUser) {
         List<Button> buttonList = new LinkedList<>();
-
         buttonList.add(ButtonIMpl.getButtonPlayAgainWithUsers(userIdLong, secondUser));
+        return getButtons(userIdLong, buttonList);
+    }
 
-        if (BotStartConfig.getMapGameLanguages().get(userIdLong).equals("eng")) {
+    @NotNull
+    private static List<Button> getButtons(long userIdLong, List<Button> buttonList) {
+        String language = BotStartConfig.getMapGameLanguages().get(userIdLong);
+        if (language != null && language.equals("eng")) {
             buttonList.add(ButtonIMpl.BUTTON_RUSSIAN);
         } else {
             buttonList.add(ButtonIMpl.BUTTON_ENGLISH);
         }
-
         buttonList.add(ButtonIMpl.BUTTON_MY_STATS);
-
         return buttonList;
     }
 }
