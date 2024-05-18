8905fa42581ce028f9160d713ad19d276022a682
megoRU <savin.yura2014@gmail.com>
2024-05-18 12:43:51 +0300

    1. Revert

--- src/main/java/main/service/UserSettingsService.java
+++ src/main/java/main/service/UserSettingsService.java
@@ -17,6 +17,7 @@ public class UserSettingsService {
     public void settings() {
         List<UserSettings> userSettingsList = userSettingsRepository.findAll();
 
+
         for (UserSettings userSettings : userSettingsList) {
             Long userIdLong = userSettings.getUserIdLong();
             UserSettings.BotLanguage botLanguage = userSettings.getBotLanguage();
